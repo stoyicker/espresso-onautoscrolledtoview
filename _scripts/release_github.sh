b9394bbff1b3b35175a5650b5516a6c9663afdbf
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2020-08-12 19:34:29 +0200

    Add _scripts

---
+++ _scripts/release_github.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+set -e
+
+uploadReleaseToGitHub() {
+  git fetch --tags
+  THIS_TAG=$(git describe --tags --abbrev=0)
+
+  BODY="{
+        \"tag_name\": \"$THIS_TAG\",
+        \"target_commitish\": \"master\",
+        \"name\": \"$THIS_TAG\",
+        \"body\": \" \"
+    }"
+
+  # Create the release in GitHub and extract its id from the response
+  RESPONSE_BODY=$(curl -s \
+    -v \
+    -u "${CIRCLE_USERNAME}":"${GITHUB_TOKEN}" \
+    --header "Accept: application/vnd.github.v3+json" \
+    --header "Content-Type: application/json; charset=utf-8" \
+    --request POST \
+    --data "${BODY}" \
+    https://api.github.com/repos/"${CIRCLE_PROJECT_USERNAME}"/"${CIRCLE_PROJECT_REPONAME}"/releases)
+
+  # Extract the upload_url value
+  UPLOAD_URL=$(echo ${RESPONSE_BODY} | jq -r .upload_url)
+
+  cp library/build/outputs/jar/library-${THIS_TAG}.jar library.jar
+
+  # Attach library
+  LIBRARY_UPLOAD_URL=$(echo ${UPLOAD_URL} | sed "s/{?name,label}/?name=library-${THIS_TAG}.jar/")
+  curl -s \
+    -v \
+    -u "${CIRCLE_USERNAME}":"${GITHUB_TOKEN}" \
+    --header "Accept: application/vnd.github.v3+json" \
+    --header "Content-Type: application/zip" \
+    --data-binary "@library.jar" \
+    --request POST \
+    "${LIBRARY_UPLOAD_URL}"
+
+  cp demo/build/outputs/apk/debug/demo-debug.apk demo.apk
+
+  # Attach demo
+  DEMO_UPLOAD_URL=$(echo ${UPLOAD_URL} | sed "s/{?name,label}/?name=demo-${THIS_TAG}.apk/")
+  curl -s \
+    -v \
+    -u "${CIRCLE_USERNAME}":"${GITHUB_TOKEN}" \
+    --header "Accept: application/vnd.github.v3+json" \
+    --header "Content-Type: application/zip" \
+    --data-binary "@demo.apk" \
+    --request POST \
+    "${DEMO_UPLOAD_URL}"
+
+  echo "GitHub release complete."
+}
